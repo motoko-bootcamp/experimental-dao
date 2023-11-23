617fb1c0ae65491f29ecce97227b78bcfd11bc4a
varonroy <varonroy@gmail.com>
2023-11-23 16:55:23 +0000

    Added the Issue type to http-trasmitter

--- http-transmitter/src/main.rs
+++ http-transmitter/src/main.rs
@@ -1,16 +1,33 @@
 use axum::{routing::post, Router};
+use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, net::SocketAddr, process::Command};
 
 const METHOD_NAME: &'static str = "add_to_log";
 
-
-struct Issue {
-    number: u64,
-    title: String,
-    body: Option<String>,
-    state: String,
+#[derive(Debug, Clone, Copy, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+enum State {
+    Closed,
+    Open,
 }
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct User {
+    login: String,
+    id: u64,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct Issue {
+    id: u64,
+    title: String,
+    number: u64,
+    user: User,
+    // issue.labels: [string] ---- ignore for now,
+    state: State,
+    created_at: String, // (date-time) (2023-11-23T15:56:41Z),
+    body: Option<String>,
+}
 
 fn get_local_backend_canister_id() -> String {
     let source = std::fs::read_to_string("../.dfx/local/canister_ids.json").unwrap();
@@ -31,11 +48,18 @@ async fn main() {
 }
 
 async fn root_post(body: String) {
+    let mut body: serde_json::Value = serde_json::from_str(&body).unwrap();
+    let issue = body["issue"].take();
+    let issue: Issue = serde_json::from_value(issue).unwrap();
+    send_to_canister(issue)
+}
+
+fn send_to_canister(arg: impl Serialize) {
+    let body = serde_json::to_string(&arg).unwrap();
     println!("sending {body} to the canister");
     let canister_id = get_local_backend_canister_id();
     let body = body.replace("\"", "\\\"");
     let body = format!("(\"{body}\")");
-    println!("{body}");
     let status = Command::new(format!("dfx"))
         .args([
             "canister",
