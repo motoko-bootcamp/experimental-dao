62b18ab1ef293fa8413cc14619791dc1dc1fe40b
Elie Gauthey <elie.gauthey@gmail.com>
2023-11-24 09:58:10 +0100

    started proposal thread local

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -1,6 +1,6 @@
 use ic_cdk_macros::{export_candid, update};
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
-use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap};
+use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap, StableCell};
 use std::cell::RefCell;
 
 use crate::{
@@ -20,10 +20,24 @@ type Memory = VirtualMemory<DefaultMemoryImpl>;
 thread_local! {
     static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
         RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
     pub static MEMBERS: RefCell<StableBTreeMap<StablePrincipal, Member, Memory>> = RefCell::new(
         StableBTreeMap::init(
             MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0))),
         )
+    );
+
+    pub static PROPOSALS: RefCell<StableBTreeMap<u64, Proposal, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(1))),
+        )
+    );
+
+    static NEXT_PROPOSAL_ID: RefCell<StableCell<u64, Memory>> = RefCell::new(
+        StableCell::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(2))),
+            0
+        ).unwrap()
     );
 
     // pub static ISSUES: RefCell<Vec<IssueData>> = RefCell::new(Vec::new());
